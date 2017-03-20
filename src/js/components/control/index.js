2de693d32416ea6370b047e475621337b62c062e
linfeng008 <klfl-0008@163.com>
2017-03-20 22:38:10 +0800

    feat: 添加播放控制

--- src/js/components/control/index.js
+++ src/js/components/control/index.js
@@ -1,25 +1,63 @@
 'use strict';
 // 播放器控制器
 
-import React, {Component} from 'react';
+import React, {
+  Component
+} from 'react';
 import './index.scss';
 
 class Control extends Component {
-    constructor(props) {
-        super(props);        
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicList: [
+        '../source/haikuotiankong.mp3',
+        '../source/guanghuisuiyue.mp3'
+      ],
+      defaultActive: 0,
+      playIng: '../source/haikuotiankong.mp3'
     }
-    componentWillMount() {
-      var self = this;
-      self.audio = document.createElement('audio');
-      self.audio.src = '../source/haikuotiankong.mp3';
-      //self.audio.src = 'http://mr1.doubanio.com/e5297b782dae1039274c53738d149334/1/fm/song/p278560_128k.mp4';
-      self.audio.autoplay = true;
-      self.audio.addEventListener('canplaythrough', function() {
-        self.audio.play();
-      });
+  }
+  componentWillMount() {
+    var self = this;
+    self.audio = document.createElement('audio');
+    self.audio.src = this.state.playIng;
+    //self.audio.src = 'http://mr1.doubanio.com/e5297b782dae1039274c53738d149334/1/fm/song/p278560_128k.mp4';
+    self.audio.autoplay = true;
+    self.audio.addEventListener('canplaythrough', function() {
+      self.audio.play();
+    });
+  }
+  componentWillUpdate(nextProps, nextState) {
+    var self = this;
+    self.audio.src = nextState.playIng;
+  }
+
+  play() {
+    var self = this;
+    if (self.audio.paused) {
+      self.audio.play();
+    } else {
+      self.audio.pause();
     }
-    render() {
-        return <div className="controls">
+  }
+
+  playPrev() {
+    var self = this;
+    self.setState({
+      playIng: this.state.musicList[1]
+    })
+  }
+
+  playNext() {
+    var self = this;
+    self.setState({
+      playIng: this.state.musicList[0]
+    })
+  }
+
+  render() {
+    return <div className="controls">
           <div className="top">
             <div className="column like">
               <span className="iconfont icon-like"></span>
@@ -33,19 +71,19 @@ class Control extends Component {
           </div>
           <div className="bottom">
               <div className="playBtn">
-                <div className="prev">
+                <div className="prev" onClick={this.playPrev.bind(this)}>
                   <span className="iconfont icon-prev"></span>
                 </div>
-                <div className="play">
+                <div className="play" onClick={this.play.bind(this)}>
                   <span className="iconfont icon-play"></span>
                 </div>
-                <div className="next">
+                <div className="next" onClick={this.playNext.bind(this)}>
                   <span className="iconfont icon-next"></span>
                 </div>
               </div>
           </div>
         </div>
-    }
+  }
 };
 
 export default Control;
